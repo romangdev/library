bf279c1cb6a6772cabf24d1147b9806c99b6bf33
Roman Geathers <romangeathersdev@gmail.com>
2022-09-09 14:12:45 -0400

    Add 'new book' button
    Add a button to allow user to add information on a new book to their
    library.
    
    Once new book is added, the display is updated to show the addition.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
 const books = document.querySelector('.books');
+const newBookButton = document.querySelector('.new-book');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -20,9 +21,17 @@ function addBookToLibrary() {
   read = prompt("Have you read the book?");
 
   myLibrary.push(new Book(title, author, pages, read));
+  displayBooks();
+}
+
+function removeAllChildNodes() {
+  while (books.firstChild) {
+    books.removeChild(books.firstChild);
+  }
 }
 
 function displayBooks() {
+  removeAllChildNodes();
   for (i = 0; i < myLibrary.length; i++) {
     let book = books.appendChild(document.createElement('p'));
     book.textContent = myLibrary[i].info();
@@ -32,3 +41,9 @@ function displayBooks() {
 myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read'));
 myLibrary.push(new Book('Test Book', 'Test Guy', '434', 'read'));
 myLibrary.push(new Book('The Alchemist', 'Paolo Coehlo', 330, 'read'));
+
+newBookButton.addEventListener('click', () => {
+  addBookToLibrary();
+});
+
+displayBooks();

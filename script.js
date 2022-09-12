db836a7fbb975ac1cf8eef22b8eb7053444c5501
Roman Geathers <romangeathersdev@gmail.com>
2022-09-11 20:56:01 -0400

    Add basic "checkRead" function
    Add an initial working version of a function that toggles the read
    status of each book in the user's library.

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ let myLibrary = [];
 
 const books = document.querySelector('.books');
 const newBookButton = document.querySelector('.new-book');
-let removeBtns = null;
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
 
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+}
+
+Book.prototype.checkRead = function() {
+  if (this.read === "read") {
+    this.read = "not read";
+  } else {
+    this.read = "read";
   }
 }
 
@@ -35,10 +42,15 @@ function displayBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
     let book = books.appendChild(document.createElement('p'));
     let removeBtn = books.appendChild(document.createElement('button'));
+    let readBtn = books.appendChild(document.createElement('button'));
 
-    removeBtn.textContent = 'remove';
+    removeBtn.textContent = 'Remove';
     removeBtn.setAttribute('data-id', i);
     removeBtn.classList.add('removeBtn');
+
+    readBtn.textContent = 'Read?';
+    readBtn.setAttribute('data-id-read', i);
+    readBtn.classList.add('readBtn');
 
     book.textContent = myLibrary[i].info();
   }
@@ -49,17 +61,18 @@ myLibrary.push(new Book('Test Book', 'Test Guy', '434', 'read'));
 myLibrary.push(new Book('The Alchemist', 'Paolo Coehlo', 330, 'read'));
 
 displayBooks();
-removeBtns = document.querySelectorAll('.removeBtn');
 
 newBookButton.addEventListener('click', () => {
   addBookToLibrary();
   displayBooks();
-  removeBtns = document.querySelectorAll('.removeBtn');
 });
 
 books.addEventListener('click', (e) => {
   if (e.target.classList.contains('removeBtn')) {
     myLibrary.splice(e.target.getAttribute('data-id'), 1);
+    displayBooks();
+  } else if (e.target.classList.contains('readBtn')) {
+    myLibrary[e.target.getAttribute('data-id-read')].checkRead();
     displayBooks();
   }
 });
